481a6dffedd59152b520ddd336489df5d748ba85
kiaracastillo <kiaracastillom@gmail.com>
2025-03-07 23:04:28 -0500

    Prompt 4 changes

---
+++ views/js/kiara.js
@@ -0,0 +1,15 @@
+//Child Class / Sub-Class	
+/* Scenario:
+
+A user selects food items from a restaurant’s menu. Users should be able to see a list of menu items (at least 2) and add them to a cart. Users can then click on a button to view the receipt and another to download the receipt.
+
+PDF Details:
+
+Show the following on the pdf:
+
+- header with a company name (name can be anything of your choice)
+- sub-heading showing date (date of generation)
+- list of menu items that user has in cart
+- each line should have product name, price, quantity, and line total
+- grand total for the receipt */
+
