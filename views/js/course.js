3e8088e789a199b28565052307e60b2a33b92c6c
fralcala <fralcala@iu.edu>
2025-03-08 23:45:12 -0500

    created quiz

--- views/js/course.js
+++ views/js/course.js
@@ -1,4 +1,28 @@
-import GeneratePdf from "./app.js";
+import { GeneratePdf } from "./app.js";
+console.log("it be workin");
+
+function checkAnswers() {
+  const answers = {
+    q1: "water",
+    q2: "yes",
+  };
+
+  let score = 0;
+
+  for (let key in answers) {
+    const selectedOption = document.querySelector(
+      `input[name="${key}"]:checked`
+    );
+    if (selectedOption && selectedOption.value === answers[key]) {
+      score++;
+    }
+  }
+
+  document.getElementById("result").innerText = `You scored ${score} out of 2!`;
+}
+
+const check = document.getElementById("submit-button");
+check.addEventListener("click", checkAnswers);
 
 class Certificate extends GeneratePdf {}
 
