3682db1d20d6c6348de0ddb86b84efa28e3aec91
fralcala <fralcala@iu.edu>
2025-03-10 23:55:03 -0400

    added pdf

--- views/js/course.js
+++ views/js/course.js
@@ -1,6 +1,8 @@
 import { GeneratePdf } from "./app.js";
 console.log("it be workin");
 
+class Certificate extends GeneratePdf {}
+
 const section = document.getElementById("section");
 
 function checkAnswers() {
@@ -26,25 +28,45 @@ function checkAnswers() {
     <h2>Congratulations, you Passed! &#127881</h2>
 
     <h3>You scored ${score} out of 2!</h3>
+
+    <div id="pdf-holder">
     
-    <label for="name">Enter your name: </label>
-    <input placeholder="Your Name" type="text" id="name" name="name">
+      <label for="name">Enter your name: </label>
+      <input placeholder="Your Name" type="text" id="name" name="name">
   
-      <button class="course-button" id="pdf-button">My Certificate</button>
+      <button id="pdf-button">My Certificate</button>
+    </div>
   </div>
   `;
+  function getName() {
+    let name = document.getElementById("name");
 
-  document.getElementById;
+    let value = name.value;
+
+    console.log(value);
+    return name;
+  }
+  const userName = document.getElementById("pdf-button");
+  userName.addEventListener("click", function () {
+    getName();
+
+    document.getElementById("pdf-holder").innerHTML = `<iframe
+          class="course-pdf"
+          id="pdf-preview"
+          src=""
+          frameborder="0"
+        ></iframe>`;
+    const myPdf = new GeneratePdf("pdf-preview");
+
+    myPdf.addHeader("Tiramisu Course");
+    myPdf.addHeader("Certificate Of Completion");
+    myPdf.addText(`To: ${name.value}`);
+
+    myPdf.showPdf();
+  });
 }
-
-/*const d = new Date("2021-03-25");
-d.getFullYear();*/
-
 const check = document.getElementById("submit-button");
 check.addEventListener("click", checkAnswers);
 
-class Certificate extends GeneratePdf {}
-
-const myPdf = new GeneratePdf("pdf-preview");
-
-myPdf.showPdf();
+/*const d = new Date("2021-03-25");
+d.getFullYear();*/
