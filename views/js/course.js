9eb161d5a6e8bd8453ce51cbe7bedad44faf884b
fralcala <fralcala@iu.edu>
2025-03-10 23:21:59 -0400

    created passed section

--- views/js/course.js
+++ views/js/course.js
@@ -1,6 +1,8 @@
 import { GeneratePdf } from "./app.js";
 console.log("it be workin");
 
+const section = document.getElementById("section");
+
 function checkAnswers() {
   const answers = {
     q1: "water",
@@ -9,6 +11,7 @@ function checkAnswers() {
 
   let score = 0;
 
+  // loops through users responses and checks if they match the right answers
   for (let key in answers) {
     const selectedOption = document.querySelector(
       `input[name="${key}"]:checked`
@@ -18,8 +21,24 @@ function checkAnswers() {
     }
   }
 
-  document.getElementById("result").innerText = `You scored ${score} out of 2!`;
+  document.getElementById("main").innerHTML = `
+  <div class="passed">
+    <h2>Congratulations, you Passed! &#127881</h2>
+
+    <h3>You scored ${score} out of 2!</h3>
+    
+    <label for="name">Enter your name: </label>
+    <input placeholder="Your Name" type="text" id="name" name="name">
+  
+      <button class="course-button" id="pdf-button">My Certificate</button>
+  </div>
+  `;
+
+  document.getElementById;
 }
+
+/*const d = new Date("2021-03-25");
+d.getFullYear();*/
 
 const check = document.getElementById("submit-button");
 check.addEventListener("click", checkAnswers);
